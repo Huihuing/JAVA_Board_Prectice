8afad81252c74901f355971072da0bc42b6b75d9
Huihuing <3826com@gmail.com>
2024-03-13 16:38:33 +0900

    java union method

---
+++ src/main/java/main/B_Update.java
@@ -0,0 +1,43 @@
+package main;
+import main.BoardApp;
+import java.util.List;
+import java.util.Scanner;
+
+public class B_Update {
+
+    private List<String> titleList;
+    private List<String> ScriptList;
+    private Scanner scan;
+
+    public B_Update(List<String> titleList, List<String> ScriptList, Scanner scan) {
+        this.titleList = titleList;
+        this.ScriptList = ScriptList;
+        this.scan = scan;
+    }
+
+    public void update() {
+        System.out.println("==================");
+        for (int i = 0; i < titleList.size(); i++) {
+            System.out.println(i + 1);
+            String title = titleList.get(i);
+            System.out.printf("제목 : %s\n", title);
+            System.out.println("==================\n");
+        }
+        System.out.print("수정할 게시물 번호 : ");
+        int upNum = scan.nextInt();
+        if (upNum < 1 || upNum > titleList.size()) { // 올바른 게시물 번호 범위 확인
+            System.out.println("없는 게시물 번호입니다.");
+        } else {
+            scan.nextLine(); // 버퍼 비우기
+            System.out.print("새로운 제목을 입력하세요 : ");
+            String newTitle = scan.nextLine();
+            titleList.set(upNum - 1, newTitle); // 수정된 제목 저장
+
+            System.out.print("새로운 내용을 입력하세요 : ");
+            String newScript = scan.nextLine();
+            ScriptList.set(upNum - 1, newScript); // 수정된 내용 저장
+
+            System.out.println("게시물이 수정되었습니다.\n");
+        }
+    }
+}
--- src/main/java/main/BoardApp.java
+++ src/main/java/main/BoardApp.java
@@ -4,24 +4,20 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Date;
-import java.awt.Frame;
-import java.awt.Label;
-import java.awt.Panel;
-import java.awt.TextField;
+import java.util.List;
 
 public class BoardApp {
     public void run() {
-        Frame f = new Frame();
-        f.setTitle("첫 번째 프레임 입니다.");
-        f.setBounds(100, 100, 300, 300);
-        f.setVisible(true);
 
+        List<String> titleList = new ArrayList<>();
+        List<String> ScriptList = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
-        SimpleDateFormat kortime = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        int bun = 0;
+        SimpleDateFormat kortime = new SimpleDateFormat("yyyy.MM.dd hh:mm:ss");
+
+        B_Update updatedata = new B_Update(titleList, ScriptList, scan);
         // 변수에는 하나의 값만 저장 가능하므로 여러개의 게시물을 저장하려면 ArrayList를 사용한다.
-        ArrayList<String> titleList = new ArrayList<>();
-        ArrayList<String> ScriptList = new ArrayList<>();
+        /*ArrayList<String> titleList = new ArrayList<>();
+        ArrayList<String> ScriptList = new ArrayList<>();*/
         ArrayList<String> DateList = new ArrayList<>();
         ArrayList<Integer> NumList = new ArrayList<>();
         ArrayList<Integer> ReadPoint = new ArrayList<>();
@@ -32,17 +28,21 @@ public class BoardApp {
         ScriptList.add("자바 공부중이에요.");
         ScriptList.add("질문좀 할게요~");
         ScriptList.add("공부중이에요.");
-        DateList.add("2024-01-01 12:12:12");
-        DateList.add("2024-01-02 12:12:12");
-        DateList.add("2024-01-03 12:12:12");
-        NumList.add(1);
-        NumList.add(2);
-        NumList.add(3);
+
+        DateList.add("2024.01.01 12:12:12");
+        DateList.add("2024.01.02 12:12:12");
+        DateList.add("2024.01.03 12:12:12");
+
 
 /*      String title = ""; // 딱히 처음에 값이 필요 없음. 추후에 입력을 통해 넣을 것임.
         String Script = "";*/
+        for (int i = 1; i < titleList.size()+1; i++) { // intater로 해주면 for문으로 더 효율적으로 하자
+            NumList.add(i);
+            ReadPoint.add(1);
+        }
 
-        ReadPoint.add(0);
+        /*ReadPoint.add(0);
+        ReadPoint.add(0);*/
         System.out.println("-------------");
         // 반복 횟수 정할 수 없음 => 무한 반복 구조
 
@@ -71,8 +71,9 @@ public class BoardApp {
                     int num = NumList.size() + 1;
                     NumList.add(num);
 
-                    System.out.println("게시물이 등록되었습니다.\n");
+                    ReadPoint.add(1);
 
+                    System.out.println("게시물이 등록되었습니다.\n");
 
                     System.out.print("다시 등록 하시겠습니까? : ");
 
@@ -101,38 +102,16 @@ public class BoardApp {
                         String title = titleList.get(i);
                         System.out.printf("제목 : %s\n", title);
 
-                        String Script = ScriptList.get(i);
+/*                        String Script = ScriptList.get(i);
 //                      System.out.printf("내용 : %s\n", Script);
 
-                        String time = DateList.get(i);
+                        String time = DateList.get(i);*/
                     System.out.println("==================");
                 }
 
 
             } else if (exit.equals("update")) {
-                System.out.println("==================");
-                for (int i = 0; i < titleList.size(); i++) {
-                    System.out.println(i + 1);
-                    String title = titleList.get(i);
-                    System.out.printf("제목 : %s\n", title);
-                    System.out.println("==================\n");
-                }
-                System.out.print("수정할 게시물 번호 : ");
-                int upNum = scan.nextInt();
-                if (upNum < 1 || upNum > titleList.size()) { // 올바른 게시물 번호 범위 확인
-                    System.out.println("없는 게시물 번호입니다.");
-                } else {
-                    scan.nextLine(); // 버퍼 비우기
-                    System.out.print("새로운 제목을 입력하세요 : ");
-                    String newTitle = scan.nextLine();
-                    titleList.set(upNum - 1, newTitle); // 수정된 제목 저장
-
-                    System.out.print("새로운 내용을 입력하세요 : ");
-                    String newScript = scan.nextLine();
-                    ScriptList.set(upNum - 1, newScript); // 수정된 내용 저장
-
-                    System.out.println("게시물이 수정되었습니다.\n");
-                }
+                updatedata.update();
 
             } else if (exit.equals("delete")) {
                 System.out.print("삭제할 게시물 번호 : ");
@@ -143,52 +122,100 @@ public class BoardApp {
                     scan.nextLine();
                     titleList.remove(deNum - 1);
                     System.out.printf("%d", deNum);
-                    System.out.printf(" 번 게시물이 삭제되었습니다.\n");
+                    System.out.print(" 번 게시물이 삭제되었습니다.\n");
                 }
-            } else if (exit.equals("detail")) {
+            }
+            else if (exit.equals("detail")) {
                 System.out.print("상세보기 할 게시물 번호를 입력해주세요 : ");
-                int detNum = scan.nextInt();
+                int detNum = scan.nextInt(); // 게시물 1부터 시작 and 찾게 하는 기능?
+                scan.nextLine();
+
                 if (detNum < 1 || detNum > titleList.size()) {
                     System.out.println("없는 게시물입니다.");
                 } else {
-                    scan.nextLine();
                     System.out.println("==================");
-                    {
-                        System.out.printf("번호 : %d\n", detNum);
 
-                        String title = titleList.get(detNum - 1);
+                        int calcul = detNum - 1;
+
+                        int num = NumList.get(calcul);
+                        System.out.printf("번호 : %d\n", num);
+
+                        String title = titleList.get(calcul);
                         System.out.printf("제목 : %s\n", title);
 
-                        String Script = ScriptList.get(detNum - 1);
+                        String Script = ScriptList.get(calcul);
                         System.out.printf("내용 : %s\n", Script);
 
-                        String time = DateList.get(detNum - 1);
+                        String time = DateList.get(calcul);
                         System.out.printf("등록 날짜 : %s\n", time);
 
-                        int read = ReadPoint.get(detNum -1);
+                        int read = ReadPoint.get(calcul);
                         System.out.printf("조회수 : %d\n", read);
 
-                    }
-                    System.out.println("==================");
-                }
+                        ReadPoint.set(calcul, read + 1);
 
+                        System.out.println("==================");
+
+                        /*String manum = "";
+                        int manu = 0;
+                        String demanu= "";*/
+
+                        System.out.print("상세보기 기능을 선택해주세요(1.댓글 등록, 2. 추천, 3. 수정, 4. 삭제, 5. 목록으로) : " );
+                        String input = scan.nextLine();
+                        /*manum = scan.nextLine();
+                        scan.nextLine();*/
+
+                    if(input.isEmpty()) {
+                        System.out.println("[잘못된 입력입니다.]");
+                    }
+                    else {
+                        int manu = Integer.parseInt(input);
+                        switch (manu)
+                        {
+                            case 1:
+                                System.out.print("\n[댓글 기능] : ");
+                                String comment = scan.nextLine();
+                                System.out.println("댓글이 성공적으로 등록되었습니다.\n");
+                                break;
+                            case 2:
+                                System.out.println("\n[추천 기능]");
+                                break;
+                            case 3:
+                                System.out.println("\n[수정 기능]");
+                                updatedata.update();
+                                break;
+                            case 4:
+                                System.out.println("\n[삭제 기능]");
+                                break;
+                            case 5:
+                                System.out.println("\n[목록으로]");
+                                break;
+                            default:
+                                System.out.println("[잘못된 입력입니다.]");
+                                break;
+                        }
+                    }
+
+
+                }
             }
             else if (exit.equals("search")) {
                 System.out.print("검색 키워드를 입력해주세요 : ");
                 String find = scan.next();
-//                boolean found = false;
-
+                boolean found = false;
+                scan.nextLine();
                 for (int i = 0; i < titleList.size(); i++) {
                     String title = titleList.get(i);
 //                    System.out.printf("제목 : %s\n", title); 여기다 쓰면 타이틀에 있는 내용들 다 나옴
                     if (title.contains(find)) {
                         System.out.printf("검색결과 - 제목 : %s\n", title);
-//                        found = true;
+                        found = true;
 
                     }
-                    else {
+                    if(!found) {
                         System.out.println("검색 결과가 없습니다.");
                     }
+
                 }
 
             }
